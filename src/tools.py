11265a95701495f2319d9cf2d31589a8b3f3b911
jamesballaro <jamesalexballaro@gmail.com>
2025-12-02 11:25:28 +0000

    FIX: Added test_name to output files

--- src/tools.py
+++ src/tools.py
@@ -1,14 +1,23 @@
 import os
-
 from langchain.tools import tool
+from functools import partial
 
-@tool("write_to_file", return_direct=True)
-def write_to_file(content: str, test_name:str, filename: str = "output.txt") -> str:
-    """
-    Writes text content to a file within the ./output directory.
-    """
-    os.makedirs("output", exist_ok=True)
-    filepath = os.path.join("output", test_name, filename)
-    with open(filepath, "w", encoding="utf-8") as f:
-        f.write(content)
-    return f"[FileTool] Output successfully written to {filepath}"
+def build_registry(test_name: str):
+    return [
+        tool(
+            name_or_callable="write_to_file",
+            runnable=make_write_to_file(test_name=test_name),
+            description="Writes content to a file in the output directory.",
+        )
+    ]
+
+#Factory method instead of functools.partial which is incompatible with langchain
+def make_write_to_file(test_name: str):
+    def inner(content: str, filename: str = "output.txt") -> str:
+        os.makedirs(os.path.join("output", test_name), exist_ok=True)
+        filepath = os.path.join("output", test_name, filename)
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(content)
+        return f"[FileTool] Output successfully written to {filepath}"
+    return inner
+
